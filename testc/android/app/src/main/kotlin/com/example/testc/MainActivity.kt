cf8b6bd9fb87844c94062bc79b0eb80f7a5f4575
Ajay.D <ajaysankardiwakar@gmail.com>
2021-05-13 08:40:00 +0530

    chat

--- testc/android/app/src/main/kotlin/com/example/testc/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.testc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- testc/lib/src/auth/auth_provider_base.dart
+++
@@ -1,7 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-
-abstract class AuthProviderBase {
-  Future<FirebaseApp> initialize();
-  Future<UserCredential> signInWithGoogle();
-}
